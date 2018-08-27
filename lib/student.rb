32b0d47d451f7b48f397a97ca7433e59c18caf1f
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 19:20:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,9 @@ class Student
      FROM students
      SQL
 
-     DB[:conn].execute(sql).map do |row0|
-
+     DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
