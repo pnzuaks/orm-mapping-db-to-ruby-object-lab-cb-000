da555354a138542fa52c9425ddb30770c260d446
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 19:28:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,6 +80,12 @@ class Student
   end
 
   def first_X_students_in_grade_10()
-    
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade < 12
+     SQL
+
+     DB[:conn].execute(sql)
   end
 end
